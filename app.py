2076f62fa856779fa17762aa0c41e74b307b37f2
yyt-gw <yeyintthu@globalwalkers.co.jp>
2023-10-20 15:15:31 +0630

    update codes for socket API

--- app.py
+++ app.py
@@ -6,48 +6,59 @@ cdate : Tuesday October 3rd 2023
 mdate : Tuesday October 3rd 2023
 copyright: 2023 GlobalWalkers.inc. All rights reserved.
 """
-from fastapi import FastAPI, WebSocket, WebSocketDisconnect
-import numpy as np
-import cv2
-import os
+import socket
+import pickle
+import struct
+import json
 from white_cane_detector.openvino_inference import WhiteCaneDetector
-import uvicorn
-from config.config import PORT, RELOAD
-from time import time
-
-# Initialize the Flask application
-app = FastAPI()
-
-white_cane_detector = WhiteCaneDetector()
+from config.config import PORT
 
 
-# cropped_images_dir = '/home/jetson1/cropped_images_dir'
-# route http posts to this method
-@app.websocket("/ws/detect_objects")
-async def detect_white_cane(websocket: WebSocket):
-    await websocket.accept()
-    try:
-        while True:
-            start_t = time()
-            img_contents = await websocket.receive_bytes()
-            # convert string of image data to uint8
-            nparr = np.frombuffer(img_contents, np.uint8)
-            # decode image
-            img = cv2.imdecode(nparr, cv2.IMREAD_UNCHANGED)
+class WhiteCaneSocketListener:
+    def __init__(self) -> None:
+        self._white_cane_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.__socket_address = ("", PORT)
+        self._white_cane_socket.bind(self.__socket_address)
+        self._white_cane_socket.listen(5)
+        print("White cane socket is now listening...")
 
-            result_json = white_cane_detector._predict(img)
-            # build a response dict to send back to client
-            response = {
-                "results": result_json,
-            }
-            await websocket.send_json(response)
-            print(f"Time taken : {round((time()-start_t)*1000, 2)} ms")
-    except WebSocketDisconnect:
-        print("Client disconnected")
+
+def main():
+    white_cane_detector = WhiteCaneDetector()
+    white_cane_socket_listener = WhiteCaneSocketListener()
+    while True:
+        client_socket, addr = white_cane_socket_listener._white_cane_socket.accept()
+        print(f"Received connection from {addr}")
+        if client_socket:
+            try:
+                # used in handling binary data from network connections
+                data = b""
+                # Q: unsigned long long integer(8 bytes)
+                payload_size = struct.calcsize("Q")
+                while True:
+                    while len(data) < payload_size:
+                        packet = client_socket.recv(4 * 1024)
+                        if not packet:
+                            break
+                        data += packet
+                    packed_msg_size = data[:payload_size]
+                    data = data[payload_size:]
+                    msg_size = struct.unpack("Q", packed_msg_size)[0]
+                    while len(data) < msg_size:
+                        data += client_socket.recv(4 * 1024)
+                    frame_data = data[:msg_size]
+                    data = data[msg_size:]
+                    frame = pickle.loads(frame_data)
+                    result_json = white_cane_detector._predict(frame)
+                    # build a response dict to send back to client
+                    response = json.dumps({"results": result_json})
+                    print(response)
+                    client_socket.sendall(bytes(response, encoding="utf-8"))
+            except:
+                print("Connection dropped by client")
+
+        client_socket.close()
 
 
 if __name__ == "__main__":
-    white_cane_port = int(os.getenv("WHITE_CANE_PORT", PORT))
-    reload_flag = os.getenv("SERVER_ENV") == "dev"
-    # start uvicorn server
-    uvicorn.run("app:app", host="0.0.0.0", port=white_cane_port, reload=reload_flag)
+    main()
