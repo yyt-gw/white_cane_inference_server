8dce9f01ebbb72d74da7709eb26b5bd281598cc6
gw-shinohara <shinohara@globalwalkers.co.jp>
2023-11-06 02:54:16 +0000

    [Update] Client test

--- client_test.py
+++ client_test.py
@@ -12,12 +12,13 @@ import requests
 
 ENDPOINT = "http://localhost:8081/api/send_image"
 if __name__ == "__main__":
-    img = cv2.imread("/home/yyt/Downloads/cropped_images/white_cane_example.jpg")
+    #img = cv2.imread("/home/yyt/Downloads/cropped_images/white_cane_example.jpg")
+    img = cv2.imread("/home/ubuntu/20231018/whitecane_3.png")
     im_encoded = cv2.imencode(".jpg", img)[1].tostring()
-    for _ in range(90):
+    for _ in range(9000):
         response = requests.post(
             ENDPOINT, data=im_encoded, headers={"content-type": "image/jpeg"}
         )
-        result_json = response.json()
-        print(json.dumps(result_json, indent=4))
-        print(response.status_code)
+        #result_json = response.json()
+        #print(json.dumps(result_json, indent=4))
+        #print(response.status_code)
